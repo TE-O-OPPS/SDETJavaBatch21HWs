a205ea0fa7ecb5029cc2fee84826b23ad9da0686
Syntax <instructor@syntaxtechs.com>
2024-10-18 11:04:28 -0400

    Week 4 D5

---
+++ src/main/java/org/example/week4/d5/e3/E3ForLoop.java
@@ -0,0 +1,8 @@
+package org.example.week4.d5.e3;
+
+public class E3ForLoop {
+    public static void main(String[] args) {
+        // Use a for loop that starts with year = 2011 and continues while year <= 2019
+        // Inside the loop, print "Hey, it is year:" and the value of year
+    }
+}
---
+++ src/main/java/org/example/week4/d5/e5/E5ForLoop.java
@@ -0,0 +1,8 @@
+package org.example.week4.d5.e5;
+
+public class E5ForLoop {
+    public static void main(String[] args) {
+        // Use a for loop that starts with i = 1 and continues while i <= 10
+        // Inside the loop, print the multiplication of 3 and i in the format "3*i=result"
+    }
+}
