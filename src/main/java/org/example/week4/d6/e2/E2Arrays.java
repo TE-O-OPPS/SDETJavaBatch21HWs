80d48bf17ce91d41bf3c883e5cccb97281bf269d
Syntax <instructor@syntaxtechs.com>
2024-10-20 13:57:06 -0400

    Week 4 D6

---
+++ src/main/java/org/example/week4/d6/e2/E2Arrays.java
@@ -0,0 +1,11 @@
+package org.example.week4.d6.e2;
+
+public class E2Arrays {
+    public static void main(String[] args) {
+        // Create an array of integers and store the values 45, 78, 12, 67, 55, 89, 23, 77, and 88
+
+
+        // Use a loop to iterate through the array and print the values at even indices followed by a space
+
+    }
+}
---
+++ src/main/java/org/example/week4/d6/e3/E3Arrays.java
@@ -0,0 +1,15 @@
+package org.example.week4.d6.e3;
+
+public class E3Arrays {
+    public static void main(String[] args) {
+        // Create an array of characters and store the values 's', 'a', 'y', 'b', 'n', 'c', 't', 'd', 'a', 'e', 'x'
+
+
+        // Print the characters at the indices that form the word "syntax"
+
+
+
+
+
+    }
+}
