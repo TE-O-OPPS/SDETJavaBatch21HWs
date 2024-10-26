769fe268a372327176d973742be5775ced1564ee
Syntax <instructor@syntaxtechs.com>
2024-10-26 10:09:30 -0400

    Week 5 D4

--- src/main/java/org/example/week5/d5/e2/E2NestedForLoops.java
+++ src/main/java/org/example/week5/d5/e2/E2NestedForLoops.java
@@ -3,7 +3,6 @@ package org.example.week5.d5.e2;
 public class E2NestedForLoops {
     public static void main(String[] args) {
         // Declare and initialize the 2D array to store the multiplication table values
-        int[][] table = new int[5][10];
 
         // Use nested loops to fill the 2D array with multiplication table values
 
