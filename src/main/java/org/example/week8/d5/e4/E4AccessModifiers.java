ff74a6785a6117ace020fec87e02bc4e8a3b5031
Asghar Nazir <asghar@syntaxtechs.com>
2024-11-15 15:11:02 -0500

    Comments added

--- src/main/java/org/example/week8/d5/e4/E4AccessModifiers.java
+++ src/main/java/org/example/week8/d5/e4/E4AccessModifiers.java
@@ -2,7 +2,11 @@ package org.example.week8.d5.e4;
 
 public class E4AccessModifiers {
     public static void main(String[] args) {
-
+// Hint: Declare a private variable for `name` and assign its value within the same class.
+// Hint: Use a default variable for `city` to ensure it is accessible within the package.
+// Hint: Declare a protected variable for `school name` to access it within the same package or subclass.
+// Hint: Use a public variable for `batch number` so it can be accessed from anywhere.
+// Hint: Create a `display` method to format and print the details using all four variables.
     }
 }
 
